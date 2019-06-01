0c5db15073036195e8a7caa28817b72e4ec570a9
Justin <sumbhodi1618@gmail.com>
2019-06-01 00:45:08 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,9 @@
 require 'pry'
 class Hash
   def keys_of(*arguments)
-    #arguments.each do |arr| 
-       #{arr}
+    arguments.each do |arr| 
+       {arr}
       #binding.pry
-    #end
+    end
   end
 end
