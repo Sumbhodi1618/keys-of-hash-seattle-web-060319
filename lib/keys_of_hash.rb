69019834b1de5fa91f77ca92220fa167b2e897a6
Justin <sumbhodi1618@gmail.com>
2019-06-01 01:07:50 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,9 @@
 require 'pry'
 class Hash
-  def keys_of(arguments,*arguments)
-       arguments
+  def keys_of(arguments,*keys)
+      keys.each do |key|
+          puts "#{keys}: #{arguments}"
       #binding.pry
+      end
   end
 end
